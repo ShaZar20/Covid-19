dd5320a3a982794e50c4134a32f326ae72e7b8bb
shitot handasi <shitotazure@gmail.com>
2020-05-26 23:20:32 +0300

    with backend

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: "mongodb+srv://root:A260196r.@logikal-iilvr.gcp.mongodb.net/orimzofim?retryWrites=true&w=majority",
+    secretOrKey: "secret"
+  };
+  
---
+++ routes/Forms.js
@@ -0,0 +1,35 @@
+'use-strict'
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose');
+const Forms = require('../models/Forms');
+
+router.get('/:time',(req,res)=>{
+    //req.params.time
+    let query = {date:req.params.time}
+    Forms.find(query,(err,doc)=>{
+        if(err){
+            console.log(err)
+            res.send(err)
+        }else{
+            console.log(doc)
+            res.send(doc)
+        }
+    })
+})
+
+router.post('/',(req,res)=>{
+    let newForm = new Forms(req.body.data)
+    newForm.save(function(err,doc){
+        if(err){
+            console.log(err)
+            res.send(err)
+        }else{
+            console.log(doc)
+            res.send(doc)
+        }
+    })
+})
+
+
+module.exports = router
